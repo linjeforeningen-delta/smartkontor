d170e695870cfa4412052ff12f2273f1b6ba9fef
jorgenhlehne <32197885+jorgenhlehne@users.noreply.github.com>
2019-04-09 10:05:01 +0200

    Added bus times for Gloshaugen

--- display/display.py
+++ display/display.py
@@ -1,4 +1,5 @@
 from tkinter import Tk, Label, Button
+import time
 
 from bussapi import getBuses
 from cantinahours import getCantinaHours
@@ -13,11 +14,22 @@ class DeltaWall:
         self.master = master
         master.title("A simple GUI")
         
+        self.clock = Label(master, text="00:00")
+        
         self.cantina_hours = Label(master, text="cantina_hours_string")
-        self.cantina_hours.pack()
+        self.cantina_hours.grid(row=1, column=0)
+        
+        self.glos_label = Label(master, text="Gløshaugen:")
+        self.glos_label.grid(row=1, column=1)
+        
+        self.glos_estimated_calls = []
+        for i in range(NUMBER_OF_CALLS):
+            individual_call = Label(master, text="Bus placeholder")
+            individual_call.grid(row=2+i, column=1)
+            self.glos_estimated_calls.append(individual_call)
 
-        self.close_button = Button(master, text="Close", command=master.quit)
-        self.close_button.pack()
+#        self.close_button = Button(master, text="Close", command=master.quit)
+#        self.close_button.pack()
         
     def showTime(self):
         time = ""#QTime.currentTime()
@@ -33,14 +45,8 @@ class DeltaWall:
         print("Bus data fetched.")
         
         
-#        for i in range(NUMBER_OF_CALLS):
-#            item_line = QTableWidgetItem(g_lines[i])
-#            item_line.setTextAlignment(Qt.AlignCenter)
-#            self.glos_bus_table.setItem(i,0,item_line)
-#            item_dest = QTableWidgetItem(g_destinations[i])
-#            self.glos_bus_table.setItem(i,1,item_dest)
-#            item_time = QTableWidgetItem(g_times[i])
-#            self.glos_bus_table.setItem(i,2,item_time)
+        for i in range(NUMBER_OF_CALLS):
+            self.glos_estimated_calls[i]['text'] = g_times[i] + " " + g_lines[i] + " " + g_destinations[i]
 #            
 #        for i in range(NUMBER_OF_CALLS):
 #            item_line = QTableWidgetItem(b_lines[i])
@@ -50,6 +56,9 @@ class DeltaWall:
 #            self.brochs_bus_table.setItem(i,1,item_dest)
 #            item_time = QTableWidgetItem(b_times[i])
 #            self.brochs_bus_table.setItem(i,2,item_time)
+        
+#    def updateClock(self):
+        
             
     def updateCantinaHours(self):
         realfag_hours, hangaren_hours = getCantinaHours()
@@ -58,9 +67,14 @@ class DeltaWall:
     def updateHourly(self): #Include everything that should be updated hourly in this function
         self.updateCantinaHours()
         self.cantina_hours.after(3600000, self.updateHourly)
+        
+    def update20s(self):
+        self.updateBuses()
+        self.glos_estimated_calls[0].after(20000, self.update20s)
 
 root = Tk()
 root.attributes("-fullscreen", True)
 deltaWall = DeltaWall(root)
+deltaWall.update20s()
 deltaWall.updateHourly()
 root.mainloop()
