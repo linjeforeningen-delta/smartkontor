de8e0c4e49d3d260205117a7708e21535b0ffbe4
jorgenhlehne <32197885+jorgenhlehne@users.noreply.github.com>
2019-04-12 17:49:57 +0200

    Made cantina hour search less hardcoded to better handle changes in layout

--- display/cantinahours.py
+++ display/cantinahours.py
@@ -8,8 +8,8 @@ Created on Thu Apr  4 19:43:05 2019
 from lxml import html
 import requests
 
-HANGAREN_INDEX = 12
-REALFAG_INDEX = 16
+HANGAREN_LIST_INDEX = 3
+REALFAG_LIST_INDEX = 4
 
 def getCantinaHours():
     
@@ -18,14 +18,34 @@ def getCantinaHours():
     
     tree = html.fromstring(page.content)
     
-    open_hours = tree.xpath('//div[@class="eat-hours"]/text()')
-    for i, c in enumerate(open_hours[REALFAG_INDEX]):
+    open_hours_raw = tree.xpath('//div[@class="eat-hours"]/text()')
+    
+    open_hours = []
+    current_search_index = 0
+    
+    for i in range(len(open_hours_raw)):
+        for j, c in enumerate(open_hours_raw[i]):
+            if c == 'd':
+                current_search_index += 1
+                open_hours.append(open_hours_raw[i])
+                break
+            
+    for i, c in enumerate(open_hours[HANGAREN_LIST_INDEX]):
         if c.isdigit():
-            time_start_index = i
+            hangaren_time_start_index = i
             break
+    
+    for i, c in enumerate(open_hours[REALFAG_LIST_INDEX]):
+        if c.isdigit():
+            realfag_time_start_index = i
+            break
+    
     realfag_hours = ""
     hangaren_hours = ""
-    for i in range(time_start_index,(time_start_index + 11)):
-        realfag_hours += open_hours[REALFAG_INDEX][i]
-        hangaren_hours += open_hours[HANGAREN_INDEX][i]
+    
+    for i in range(hangaren_time_start_index,(hangaren_time_start_index + 11)):
+        hangaren_hours += open_hours[HANGAREN_LIST_INDEX][i]
+        
+    for i in range(realfag_time_start_index,(realfag_time_start_index + 11)):    
+        realfag_hours += open_hours[REALFAG_LIST_INDEX][i]
     return realfag_hours, hangaren_hours
